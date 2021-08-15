3a355105fb5c8d81ab80049539b4174260220983
anthonycc <anthonyccardozo@gmail.com>
2021-08-15 16:44:15 -0400

    created tables & schema

--- app.js
+++ app.js
@@ -3,4 +3,3 @@ const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 
-
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO departments (name, dept_id)
+    -> VALUES 
+    -> ('Sales', 1),
+    -> ('Accounting', 2),
+    -> ('Human Resources', 3);
+
+
+
